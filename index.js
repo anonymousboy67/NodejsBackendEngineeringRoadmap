91449a025f2f4c5434802c2dcc2292ece1f22fb7
anonymousboy67 <aashishad67@gmail.com>
2025-06-24 15:54:00 +0545

    understanding the event loop

---
+++ index.js
@@ -0,0 +1,16 @@
+// Understanding the event loop
+console.log('Start');
+
+setTimeout(() => {
+    console.log('Timeout callback');
+}, 0);
+
+setImmediate(() => {
+    console.log('Immediate callback');
+});
+
+process.nextTick(() => {
+    console.log('Next tick callback');
+});
+
+console.log('End');
